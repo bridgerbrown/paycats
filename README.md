71ea19d50bf3e854873716e3c74c89a118e4f135
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-01-17 16:08:41 -0800

    nav styling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## PayCats - Paypal/Venmo clone
+![PayCats logo](https://github.com/bridgerbrown/paycats/blob/main/public/paycats-logo-git.jpg?raw=true)
+
 This is a solo project of a demo Paypal/Venmo clone involving a user database of cats to make transactions with!
 
 Using React, Typescript, NextJS, Tailwind CSS, Firebase, and Firestore!
-
-![PayCats logo](https://github.com/bridgerbrown/paycats/blob/main/public/paycats-logo-git.jpg?raw=true)
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Navbar() {
+    const navItemStyle: string = "px-4 transition border-transparent border hover:border hover:transition hover:border-white hover:mx-2 active:border rounded-full px-5 py-2 mx-2"
+
     return (
         <nav className="flex py-7 bg-blue-900 w-screen">
             <Image
@@ -12,20 +14,23 @@ export default function Navbar() {
                 className='ml-5'
             />
             <div className='flex items-center text-white px-6 text-base font-Hind font-normal tracking-wide'>
-                <Link href="/pay-request" className='bg-white text-blue-900 px-5 font-semibold border-2 border-white rounded-full py-2'>
+                <Link href="/pay-request" className='mr-4 bg-white text-blue-900 px-5 font-semibold border-2 border-white rounded-full py-2'>
                     Pay/Request
                 </Link>
-                <Link href="/" className='px-6'>
+                <Link href="/" className={navItemStyle}>
                     Home
                 </Link>
-                <Link href="/profile" className='px-6'>
+                <Link href="/profile" className={navItemStyle}>
                     Profile
                 </Link>
-                <Link href="/wallet" className='px-6'>
+                <Link href="/wallet" className={navItemStyle}>
                     Wallet
                 </Link>
-                <Link href="/search" className='px-6'>
+                <Link href="/search" className={navItemStyle}>
                     Search
+                </Link>
+                <Link href="/search" className={navItemStyle}>
+                    About
                 </Link>
             </div>
         </nav>
