bcf78337180378462105ccef7455e4e8240d7900
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-12 15:32:34 -0700

    fb

--- components/firebase/firebase-keys.tsx
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBZ_0GhK2kfY3XsvUekWR3-hFKCm9QGPwQ",
-    authDomain: "paycats-7e0d4.firebaseapp.com",
-    projectId: "paycats-7e0d4",
-    storageBucket: "paycats-7e0d4.appspot.com",
-    messagingSenderId: "669155624610",
-    appId: "1:669155624610:web:5b53d5abb708fe8c5539fb",
-    measurementId: "G-ZJ3ME95DT1"
-  };
--- components/firebase/firebase.config.tsx
+++ components/firebase/firebase.config.tsx
@@ -1,8 +1,18 @@
+require('dotenv').config();
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { firebaseConfig } from "./firebase-keys";
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
