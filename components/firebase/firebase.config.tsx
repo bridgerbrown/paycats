7ec2aab54203c0425dd812c7a220305d151e7aa7
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-24 13:44:12 -0700

    PAYCATS_

--- components/firebase/firebase.config.tsx
+++ components/firebase/firebase.config.tsx
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.PAYCATS_API_KEY,
+  authDomain: process.env.PAYCATS_AUTH_DOMAIN,
+  projectId: process.env.PAYCATS_PROJECT_ID,
+  storageBucket: process.env.PAYCATS_STORAGE_BUCKET,
+  messagingSenderId: process.env.PAYCATS_MESSAGING_SENDER_ID,
+  appId: process.env.PAYCATS_APP_ID,
+  measurementId: process.env.PAYCATS_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
