af46bc06abdb23106175df615d4fa93d739657b9
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-28 19:27:28 -0700

    jest ts setup, dealing with bug

---
+++ __test__/TransactionsSection.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TransactionsSection from '@/components/transactions/TransactionsSection';
+import TransactionCard from '@/components/transactions/TransactionCard';
+import { transactions } from '@/data/defaultTransactions';
+
+describe('TransactionsSection component', () => {
+  it('renders TransactionCard components for each transaction', () => {
+    const { getAllByTestId } = render(<TransactionsSection transactions={transactions} />);
+    const transactionCards = getAllByTestId('transaction-card');
+    expect(transactionCards.length).toBe(transactions.length);
+  })
+  describe('TransactionCard', () => {
+  })
+})
+
+
--- firebase.test.js
+++
@@ -1,6 +0,0 @@
-const { initializeTestApp, initializeAdminApp, getFirestore } = require('@firebase/testing');
-require('dotenv').config();
-const admin = initializeAdminApp({ projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId });
-const db = getFirestore(admin);
-
-module.exports = { admin, db };
